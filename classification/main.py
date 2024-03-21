2eddb10f8febd2cc0ee61010470d64b89f13430b
Le Hoang Dung <hoangdungle36@gmail.com>
2024-03-21 21:32:29 +0700

    + dataset debug 27

--- classification/main.py
+++ classification/main.py
@@ -397,6 +397,68 @@ def train_one_epoch(config, model, criterion, data_loader, optimizer, epoch, mix
 #     logger.info(f' * Acc@1 {acc1_meter.avg:.3f} Acc@5 {acc5_meter.avg:.3f}')
 #     return acc1_meter.avg, acc5_meter.avg, loss_meter.avg
 
+# @torch.no_grad()
+# def validate(config, data_loader, model):
+#     criterion = torch.nn.CrossEntropyLoss()
+#     model.eval()
+
+#     batch_time = AverageMeter()
+#     loss_meter = AverageMeter()
+#     acc1_meter = AverageMeter()
+#     acc5_meter = AverageMeter()
+#     pred_probs_list = []  # List to store predicted probabilities
+#     targets_list = []  # List to store ground truth labels
+
+#     end = time.time()
+#     for idx, (images, target) in enumerate(data_loader):
+#         images = images.cuda(non_blocking=True)
+#         target = target.cuda(non_blocking=True)
+
+#         # compute output
+#         with torch.cuda.amp.autocast(enabled=config.AMP_ENABLE):
+#             output = model(images)
+
+#         # measure accuracy and record loss
+#         loss = criterion(output, target)
+#         acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+#         acc1 = reduce_tensor(acc1)
+#         acc5 = reduce_tensor(acc5)
+#         loss = reduce_tensor(loss)
+
+#         loss_meter.update(loss.item(), target.size(0))
+#         acc1_meter.update(acc1.item(), target.size(0))
+#         acc5_meter.update(acc5.item(), target.size(0))
+
+#         # Append predicted probabilities and ground truth labels
+#         pred_probs_list.append(output.detach().cpu().numpy())
+#         targets_list.append(target.detach().cpu().numpy())
+
+#         # measure elapsed time
+#         batch_time.update(time.time() - end)
+#         end = time.time()
+
+#         if idx % config.PRINT_FREQ == 0:
+#             memory_used = torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)
+#             logger.info(
+#                 f'Test: [{idx}/{len(data_loader)}]\t'
+#                 f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+#                 f'Loss {loss_meter.val:.4f} ({loss_meter.avg:.4f})\t'
+#                 f'Acc@1 {acc1_meter.val:.3f} ({acc1_meter.avg:.3f})\t'
+#                 f'Acc@5 {acc5_meter.val:.3f} ({acc5_meter.avg:.3f})\t'
+#                 f'Mem {memory_used:.0f}MB')
+
+#     # Combine predictions and ground truth labels
+#     dataPRED = np.concatenate(pred_probs_list, axis=0)
+#     dataGT = np.concatenate(targets_list, axis=0)
+
+#     # Compute ROC AUC
+#     auc_result = computeAUROC(dataPRED, dataGT)
+
+#     # logger.info(f' * Acc@1 {acc1_meter.avg:.3f} Acc@5 {acc5_meter.avg:.3f} ROC AUC {auc_result:.3f}')
+#     print("ROC AUC", auc_result)
+#     return acc1_meter.avg, acc5_meter.avg, loss_meter.avg, auc_result
+
 @torch.no_grad()
 def validate(config, data_loader, model):
     criterion = torch.nn.CrossEntropyLoss()
@@ -406,8 +468,7 @@ def validate(config, data_loader, model):
     loss_meter = AverageMeter()
     acc1_meter = AverageMeter()
     acc5_meter = AverageMeter()
-    pred_probs_list = []  # List to store predicted probabilities
-    targets_list = []  # List to store ground truth labels
+    auc_meter = AverageMeter()  # Thêm AverageMeter cho ROC AUC
 
     end = time.time()
     for idx, (images, target) in enumerate(data_loader):
@@ -430,10 +491,10 @@ def validate(config, data_loader, model):
         acc1_meter.update(acc1.item(), target.size(0))
         acc5_meter.update(acc5.item(), target.size(0))
 
-        # Append predicted probabilities and ground truth labels
-        pred_probs_list.append(output.detach().cpu().numpy())
-        targets_list.append(target.detach().cpu().numpy())
-
+        # Calculate ROC AUC for the current batch
+        auc_batch = computeAUROC(output, target)
+        # auc_meter.update(auc_batch, target.size(0))
+        print("auc batch:", auc_batch)
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
@@ -448,16 +509,8 @@ def validate(config, data_loader, model):
                 f'Acc@5 {acc5_meter.val:.3f} ({acc5_meter.avg:.3f})\t'
                 f'Mem {memory_used:.0f}MB')
 
-    # Combine predictions and ground truth labels
-    dataPRED = np.concatenate(pred_probs_list, axis=0)
-    dataGT = np.concatenate(targets_list, axis=0)
-
-    # Compute ROC AUC
-    auc_result = computeAUROC(dataPRED, dataGT)
-
-    # logger.info(f' * Acc@1 {acc1_meter.avg:.3f} Acc@5 {acc5_meter.avg:.3f} ROC AUC {auc_result:.3f}')
-    print("ROC AUC", auc_result)
-    return acc1_meter.avg, acc5_meter.avg, loss_meter.avg, auc_result
+    logger.info(f' * Acc@1 {acc1_meter.avg:.3f} Acc@5 {acc5_meter.avg:.3f} AUC {auc_meter.avg:.3f}')
+    return acc1_meter.avg, acc5_meter.avg, loss_meter.avg, auc_meter.avg
 
 
 @torch.no_grad()
