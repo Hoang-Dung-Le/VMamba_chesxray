d163cc5cccc6511d4f35f690e3c11c1de45eccde
Le Hoang Dung <hoangdungle36@gmail.com>
2024-03-21 23:02:58 +0700

    + dataset debug 37

--- classification/utils/utils.py
+++ classification/utils/utils.py
@@ -62,6 +62,7 @@ def load_pretrained_ema(config, model, logger, model_ema: ModelEma=None):
     if 'model' in checkpoint:
         # msg = model.load_state_dict(checkpoint['model'], strict=False)
         # logger.warning(msg)
+        print(checkpoint.keys())
         checkpoint_state_dict = checkpoint['state_dict']
         model_state_dict = model.state_dict()
 
