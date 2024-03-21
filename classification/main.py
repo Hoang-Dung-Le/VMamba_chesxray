bed4277934f68186d8e7c69e483dd7253e213393
Le Hoang Dung <hoangdungle36@gmail.com>
2024-03-21 21:19:42 +0700

    + dataset debug 24

--- classification/main.py
+++ classification/main.py
@@ -249,7 +249,7 @@ def train_one_epoch(config, model, criterion, data_loader, optimizer, epoch, mix
 
         with torch.cuda.amp.autocast(enabled=config.AMP_ENABLE):
             outputs = model(samples)
-        # print(outputs)
+        print(outputs)
         # print(targets)
         loss = criterion(outputs, targets)
         loss = loss / config.TRAIN.ACCUMULATION_STEPS
